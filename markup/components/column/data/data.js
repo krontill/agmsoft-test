0a1c0356e4f154f4a69c1be82cae0dd6ce4db520
krontill <till.work@gmail.com>
2017-05-23 09:39:53 +0300

    Agm soft test done.

---
+++ markup/components/column/data/data.js
@@ -0,0 +1,14 @@
+const data = {
+    column: {
+        left: {
+            class: 'column--left',
+            content: true
+        },
+        center: {
+            class: 'column--center'
+        },
+        right: {
+            class: 'column--right'
+        }
+    }
+};
